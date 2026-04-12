5483777e66c39f9624da02b24e95f89505609b78
Joositoo <jmepk115@gmail.com>
2026-04-12 23:42:47 +0200

    Add CRUD for categories

---
+++ mi-hucha/src/app/api/categorias/[id]/route.js
@@ -0,0 +1,72 @@
+import pool from '../../../../lib/db';
+import { getToken } from 'next-auth/jwt';
+
+export async function PUT(request, { params }) {
+    try {
+        const { id } = await params;
+        const { nombre, tipo } = await request.json();
+
+        const token = await getToken({ req: request, secret: process.env.NEXTAUTH_SECRET });
+        /*if (!token) {
+            return Response.json(
+                { error: 'No autorizado' },
+                { status: 401 }
+            );
+        }*/
+
+        const user_id = 1//token.id;
+
+        if (!nombre || !tipo || (tipo !== 'ingreso' && tipo !== 'gasto')) {
+            return Response.json(
+                { error: 'Faltan campos obligatorios o tipo inválido' },
+                { status: 400 }
+            );
+        }
+
+        await pool.query('UPDATE categorias SET nombre = ?, tipo = ? WHERE id = ? AND usuario_id = ?',
+            [nombre, tipo, id, user_id]
+        );
+
+        return Response.json(
+            { mensaje: 'Categoría actualizada' },
+            {status: 200 }
+        );
+    }
+    catch (error) {
+        console.error(error);
+        return Response.json(
+            { error: 'Error interno del servidor' },
+            { status: 500 }
+        );
+    }
+}
+
+export async function DELETE(request, {params }) {
+    try {
+        const { id } = await params;
+        const token = await getToken({ req: request, secret: process.env.NEXTAUTH_SECRET });
+        /*if (!token) {
+            return Response.json(
+                { error: 'No autorizado' },
+                { status: 401 }
+            );
+        }*/
+
+        const user_id = 1//token.id;
+        await pool.query('DELETE FROM categorias WHERE id = ? AND usuario_id = ?',
+            [id, user_id]
+        );
+
+        return Response.json(
+            { mensaje: 'Categoría eliminada' },
+            { status: 200 }
+        );
+    }
+    catch (error) {
+        console.error(error);
+        return Response.json(
+            { error: 'Error interno del servidor' },
+            { status: 500 }
+        );
+    }
+}
---
+++ mi-hucha/src/app/api/categorias/route.js
@@ -0,0 +1,70 @@
+import pool from '../../../lib/db';
+import { getToken } from 'next-auth/jwt';
+
+export async function GET(request) {
+    try {
+        const token = await getToken({ req: request, secret: process.env.NEXTAUTH_SECRET });
+        /*if (!token) {
+            return Response.json(
+                { error: 'No autorizado' },
+                { status: 401 }
+            );
+        }*/
+
+        const user_id = token.id;
+
+        const [ingresos] = await pool.query('SELECT * FROM categorias WHERE usuario_id = ? AND tipo="ingreso"',
+            [user_id]
+        );
+        const [gastos] = await pool.query('SELECT * FROM categorias WHERE usuario_id = ? AND tipo="gasto"',
+            [user_id]
+        );
+
+        return Response.json({ ingresos, gastos });
+    }
+    catch (error) {
+        console.error(error);
+        return Response.json(
+            { error: 'Error interno del servidor' },
+            { status: 500 }
+        );
+    }
+}
+
+export async function POST(request) {
+    try {
+        const token = await getToken({ req: request, secret: process.env.NEXTAUTH_SECRET });
+        /*if (!token) {
+            return Response.json(
+                { error: 'No autorizado' },
+                { status: 401 }
+            );
+        }*/
+
+        const user_id = 1//token.id;
+
+        const { nombre, tipo } = await request.json();
+
+        if (!nombre || !tipo || (tipo !== 'ingreso' && tipo !== 'gasto')) {
+            return Response.json(
+                { error: 'Faltan campos obligatorios o tipo inválido' },
+                { status: 400 }
+            );
+        }
+
+        const [result] = await pool.query('INSERT INTO categorias (usuario_id, nombre, tipo) VALUES (?, ?, ?)',
+            [user_id, nombre, tipo]);
+
+        return Response.json(
+            { id: result.insertId, nombre, tipo, mensaje: 'Categoría creada' },
+            { status: 201 }
+        );
+    }
+    catch (error) {
+        console.error(error);
+        return Response.json(
+            { error: 'Error interno del servidor' },
+            { status: 500 }
+        );
+    }
+}
